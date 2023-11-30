48d814b41ad26a48d9221b15c335f02a438dfe3e
XxSojasxX <sojasvaca@gmail.com>
2023-11-30 08:44:55 -0500

    Comentarios

--- lib/src/pages/save_page.dart
+++ lib/src/pages/save_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, constant_identifier_names, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 
@@ -19,22 +19,35 @@ class SavePage extends StatelessWidget {
 class _FormSave extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-        child: Column(
-      children: <Widget>[
-        TextFormField(
-          decoration: InputDecoration(
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(50)))),
-        ),
-        SizedBox(
-          height: 15,
-        ),
-        TextFormField(
-          decoration: InputDecoration(
-              labelText: "Contenido", border: OutlineInputBorder()),
-        )
-      ],
-    ));
+    return Container(
+      padding: EdgeInsets.all(20),
+      child: Form(
+          child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+                labelText: "Titulo",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)))),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          TextFormField(
+            //Agregar Mas Propiedades
+            maxLines: 15,
+            maxLength: 200,
+            validator: (value) {
+              if (value!.isEmpty) {
+                return "Tiene que ingresar datos";
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+                labelText: "Contenido", border: OutlineInputBorder()),
+          )
+        ],
+      )),
+    );
   }
 }
